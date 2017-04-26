2eccb984afff06e9591f6cbe469f6afd5d714784
Michael Fenwick <mickyfen17@aol.com>
2017-04-26 16:19:52 -0700

    build for heroku with prod webpack

--- app/bundle.js
+++ app/bundle.js
@@ -34,7 +34,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "/app/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
@@ -29922,13 +29922,12 @@
 	        alt: 'movie poster of ' + title
 	      })
 	    ),
-	    _react2.default.createElement(
-	      'button',
-	      { className: 'btn red rounded', onClick: function onClick() {
-	          return addMovieToFavorites();
-	        } },
-	      'FAVORITE'
-	    )
+	    _react2.default.createElement('button', {
+	      className: 'btn red rounded',
+	      onClick: function onClick() {
+	        return addMovieToFavorites();
+	      }
+	    })
 	  );
 	};
 	
@@ -30722,7 +30721,7 @@
 	var content = __webpack_require__(292);
 	if(typeof content === 'string') content = [[module.id, content, '']];
 	// add the styles to the DOM
-	var update = __webpack_require__(306)(content, {});
+	var update = __webpack_require__(308)(content, {});
 	if(content.locals) module.exports = content.locals;
 	// Hot Module Replacement
 	if(false) {
@@ -30752,7 +30751,7 @@
 	exports.push([module.id, "@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700);", ""]);
 	
 	// module
-	exports.push([module.id, "body {\n  font-family: 'Source Sans Pro', sans-serif;\n  background: #000000;\n  background: -webkit-linear-gradient(to left, #000000 , #434343);\n  background: linear-gradient(to left, #000000 , #434343);\n}\n\nh1 {\n  color: white;\n  text-align: center;\n}\n\na {\n  text-decoration: none;\n}\n\nnav {\n  color: white;\n  margin: 20px 0;\n  top: 0;\n  background: #fa5a5a;\n  padding: 10px 0;\n  width: 100%;\n}\n\nnav a {\n  color: white;\n  margin-right: 10px;\n}\n\nh3 {\n  color: #fa5a5a;\n  text-transform: uppercase;\n  margin-top: 20px;\n  font-size: 20px;\n}\n\n#main h1 {\n  font-size: 40px;\n  color: white;\n  border-radius: 10px;\n  margin: 10px 0;\n}\n\n#main {\n  text-align: center;\n}\n\n.selected {\n  background-color: #fa5a5a;\n  font-weight: 600;\n  font-size: 20px;\n}\n\n.movie-wrapper {\n  display: flex;\n  flex-wrap: wrap;\n  justify-content: center;\n  align-items: center;\n}\n\n.movie-poster {\n  box-shadow: 2px 2px 2px black;\n  margin-bottom: 20px;\n}\n\n.movie-poster:hover {\n  box-shadow: 2px 2px 2px black;\n  margin-bottom: 20px;\n}\n\n.movie-card {\n  width: 90%;\n  max-width: 600px;\n  margin: 0 auto;\n}\n\n.movie-card img {\n  height: 350px;\n}\n\n.movie-card p {\n  color: white;\n  line-height: 22px;\n  margin-top: 5px;\n}\n\n.movie-display {\n  position: relative;\n}\n\n.movie-wrapper {\n  display: flex;\n  flex-direction: column;\n  justify-content: center;\n  align-items: center;\n}\n\n.movie-poster {\n  width: 200px;\n}\n\n.film-reel {\n  width: 40px;\n}\n\n@media only screen and (min-width: 500px)  {\n\n  .movie-wrapper {\n    flex-direction: row;\n  }\n\n  .movie-display {\n    position: relative;\n    width: 200px;\n    margin: 0 auto;\n  }\n\n  .favorites-disp {\n    display: flex;\n    flex-wrap: wrap;\n    margin-top: 10px;\n  }\n\n}\n", ""]);
+	exports.push([module.id, "body {\n  font-family: 'Source Sans Pro', sans-serif;\n  background: #000000;\n  background: -webkit-linear-gradient(to left, #000000 , #434343);\n  background: linear-gradient(to left, #000000 , #434343);\n}\n\nh1 {\n  color: white;\n  text-align: center;\n}\n\na {\n  text-decoration: none;\n}\n\nnav {\n  color: white;\n  margin: 20px 0;\n  top: 0;\n  background: #fa5a5a;\n  padding: 10px 0;\n  width: 100%;\n}\n\nnav a {\n  color: white;\n  margin-right: 10px;\n}\n\nh3 {\n  color: #fa5a5a;\n  text-transform: uppercase;\n  margin-top: 20px;\n  font-size: 20px;\n}\n\n#main h1 {\n  font-size: 40px;\n  color: white;\n  border-radius: 10px;\n  margin: 10px 0;\n}\n\n#main {\n  text-align: center;\n}\n\n.selected {\n  background-color: #fa5a5a;\n  font-weight: 600;\n  font-size: 20px;\n}\n\n.movie-wrapper {\n  display: flex;\n  flex-wrap: wrap;\n  justify-content: center;\n  align-items: center;\n}\n\n.movie-poster {\n  width: 200px;\n  box-shadow: 2px 2px 2px black;\n  margin-bottom: 20px;\n}\n\n.movie-poster:hover {\n  box-shadow: 2px 2px 2px black;\n  margin-bottom: 20px;\n}\n\n.movie-card {\n  width: 90%;\n  max-width: 600px;\n  margin: 0 auto;\n}\n\n.movie-card img {\n  height: 350px;\n}\n\n.movie-card p {\n  color: white;\n  line-height: 22px;\n  margin-top: 5px;\n}\n\n.movie-display {\n  position: relative;\n  margin: 0 auto;\n  width: 200px;\n}\n\n.movie-wrapper {\n  display: flex;\n  flex-direction: column;\n  justify-content: center;\n  align-items: center;\n}\n\n.film-reel {\n  width: 40px;\n}\n\n@media only screen and (min-width: 500px)  {\n\n  .movie-wrapper {\n    flex-direction: row;\n  }\n\n  .movie-display {\n    position: relative;\n    width: 200px;\n    margin: 0 auto;\n  }\n\n  .favorites-disp {\n    display: flex;\n    flex-wrap: wrap;\n    margin-top: 10px;\n  }\n\n}\n", ""]);
 	
 	// exports
 
@@ -32939,13 +32938,25 @@
 	
 	
 	// module
-	exports.push([module.id, ".btn.red {\n    box-shadow: 0px 4px 0px #E04342;\n}\n\n.btn.red {\n    background-color: #fa5a5a;\n}\n\n.btn {\n    border: 0;\n    padding: 5px 15px;\n    color: #fff;\n    position: absolute;\n    right: 0;\n}\n\n.rounded {\n    border-radius: 5px;\n}\n\n.btn:active {\n\ttop: 4px;\n}\n\n.btn.red {\n  box-shadow:0px 4px 0px #E04342;\n}\n\n.btn.red:active {\n  box-shadow: 0 0 #ff4c4b;\n  background-color: #ff4c4b;\n}\n\n.submit-btn,\n.nav-button {\n  padding: 10px;\n  border: 2px solid #fff;\n  color: #FFF;\n  background-color: transparent;\n  text-transform: uppercase;\n  font-weight: 700;\n}\n\n.nav-button {\n  margin: 5px;\n}\n\n.submit-btn:hover {\n  background-color: #fa5a5a;\n  color: #fff;\n}\n\n.nav-button:hover {\n  background-color: #FFF;\n  color: #fa5a5a;\n}\n", ""]);
+	exports.push([module.id, ".btn.red {\n    box-shadow: 0px 4px 0px #E04342;\n}\n\n.btn.red {\n    background-color: #fa5a5a;\n}\n\n.btn {\n  position: absolute;\n  right: 0;\n  padding: 5px 15px;\n  border: 0;\n  height: 25px;\n  width: 25px;\n  background: #fa5a5a url(" + __webpack_require__(306) + ") no-repeat 5px 2px / 20px;\n  color: #fff;\n}\n\n.rounded {\n    border-radius: 5px;\n}\n\n.btn:active {\n\ttop: 4px;\n}\n\n.btn:focus {\n  background: #fa5a5a url(" + __webpack_require__(307) + ") no-repeat 5px 2px / 20px;\n}\n\n.btn.red {\n  box-shadow:0px 4px 0px #E04342;\n}\n\n.btn.red:active {\n  box-shadow: 0 0 #ff4c4b;\n  background-color: #ff4c4b;\n}\n\n.submit-btn,\n.nav-button {\n  padding: 10px;\n  border: 2px solid #fff;\n  color: #FFF;\n  background-color: transparent;\n  text-transform: uppercase;\n  font-weight: 700;\n}\n\n.submit-btn {\n  margin: 15px 0;\n}\n\n.nav-button {\n  margin: 5px;\n}\n\n.submit-btn:hover {\n  background-color: #fa5a5a;\n  color: #fff;\n}\n\n.nav-button:hover {\n  background-color: #FFF;\n  color: #fa5a5a;\n}\n", ""]);
 	
 	// exports
 
 
 /***/ },
 /* 306 */
+/***/ function(module, exports) {
+
+	module.exports = "\"data:image/svg+xml,%3C?xml version='1.0' encoding='iso-8859-1'?%3E %3C!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0) --%3E %3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1' id='Capa_1' x='0px' y='0px' viewBox='0 0 49.94 49.94' style='enable-background:new 0 0 49.94 49.94;' xml:space='preserve' width='512px' height='512px'%3E %3Cpath d='M48.856,22.731c0.983-0.958,1.33-2.364,0.906-3.671c-0.425-1.307-1.532-2.24-2.892-2.438l-12.092-1.757 c-0.515-0.075-0.96-0.398-1.19-0.865L28.182,3.043c-0.607-1.231-1.839-1.996-3.212-1.996c-1.372,0-2.604,0.765-3.211,1.996 L16.352,14c-0.23,0.467-0.676,0.79-1.191,0.865L3.069,16.623C1.71,16.82,0.603,17.753,0.178,19.06 c-0.424,1.307-0.077,2.713,0.906,3.671l8.749,8.528c0.373,0.364,0.544,0.888,0.456,1.4L8.224,44.702 c-0.232,1.353,0.313,2.694,1.424,3.502c1.11,0.809,2.555,0.914,3.772,0.273l10.814-5.686c0.461-0.242,1.011-0.242,1.472,0 l10.815,5.686c0.528,0.278,1.1,0.415,1.669,0.415c0.739,0,1.475-0.231,2.103-0.688c1.111-0.808,1.656-2.149,1.424-3.502 L39.651,32.66c-0.088-0.513,0.083-1.036,0.456-1.4L48.856,22.731z M37.681,32.998l2.065,12.042c0.104,0.606-0.131,1.185-0.629,1.547 c-0.499,0.361-1.12,0.405-1.665,0.121l-10.815-5.687c-0.521-0.273-1.095-0.411-1.667-0.411s-1.145,0.138-1.667,0.412l-10.813,5.686 c-0.547,0.284-1.168,0.24-1.666-0.121c-0.498-0.362-0.732-0.94-0.629-1.547l2.065-12.042c0.199-1.162-0.186-2.348-1.03-3.17 L2.48,21.299c-0.441-0.43-0.591-1.036-0.4-1.621c0.19-0.586,0.667-0.988,1.276-1.077l12.091-1.757 c1.167-0.169,2.176-0.901,2.697-1.959l5.407-10.957c0.272-0.552,0.803-0.881,1.418-0.881c0.616,0,1.146,0.329,1.419,0.881 l5.407,10.957c0.521,1.058,1.529,1.79,2.696,1.959l12.092,1.757c0.609,0.089,1.086,0.491,1.276,1.077 c0.19,0.585,0.041,1.191-0.4,1.621l-8.749,8.528C37.866,30.65,37.481,31.835,37.681,32.998z' fill='%23FFFFFF'/%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3C/svg%3E\""
+
+/***/ },
+/* 307 */
+/***/ function(module, exports) {
+
+	module.exports = "\"data:image/svg+xml,%3C?xml version='1.0' encoding='iso-8859-1'?%3E %3C!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0) --%3E %3Csvg version='1.1' id='Capa_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 47.94 47.94' style='enable-background:new 0 0 47.94 47.94;' xml:space='preserve'%3E %3Cpath style='fill:%23FFD700;' d='M26.285,2.486l5.407,10.956c0.376,0.762,1.103,1.29,1.944,1.412l12.091,1.757 c2.118,0.308,2.963,2.91,1.431,4.403l-8.749,8.528c-0.608,0.593-0.886,1.448-0.742,2.285l2.065,12.042 c0.362,2.109-1.852,3.717-3.746,2.722l-10.814-5.685c-0.752-0.395-1.651-0.395-2.403,0l-10.814,5.685 c-1.894,0.996-4.108-0.613-3.746-2.722l2.065-12.042c0.144-0.837-0.134-1.692-0.742-2.285l-8.749-8.528 c-1.532-1.494-0.687-4.096,1.431-4.403l12.091-1.757c0.841-0.122,1.568-0.65,1.944-1.412l5.407-10.956 C22.602,0.567,25.338,0.567,26.285,2.486z'/%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3Cg%3E %3C/g%3E %3C/svg%3E\""
+
+/***/ },
+/* 308 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/*
@@ -32982,7 +32993,7 @@
 		singletonElement = null,
 		singletonCounter = 0,
 		styleElementsInsertedAtTop = [],
-		fixUrls = __webpack_require__(307);
+		fixUrls = __webpack_require__(309);
 	
 	module.exports = function(list, options) {
 		if(false) {
@@ -33241,7 +33252,7 @@
 
 
 /***/ },
-/* 307 */
+/* 309 */
 /***/ function(module, exports) {
 
 	
